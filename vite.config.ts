b4de64979e288b9e7e248c5849c0533ed3cdb2d0
lildibbb <adib.asyraaf760@gmail.com>
2025-02-07 01:48:47 +0800

    feat: add config

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,19 @@ import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import path from "node:path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [viteReact(), TanStackRouterVite()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: "0.0.0.0", // Allows connections from any IP
+    port: 5173, // Ensure this matches your Nginx proxy_pass
+    strictPort: true,
+    cors: true, // Allow cross-origin requests
+    allowedHosts: ["adibasyraaf.me", "www.adibasyraaf.me"], // Allow your domain
   },
 });
